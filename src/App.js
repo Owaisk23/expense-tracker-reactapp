0611001de39a7889c5eb6666846ca2c59f354326
Owais Ahmed Khan <owais1566@gmail.com>
2020-06-16 22:21:57 +0000

    Updating App.js
    Calling components in app.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './store/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-    <div >
-      Hello world form Owais!!
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <IncomeExpenses />
+        <Balance />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
